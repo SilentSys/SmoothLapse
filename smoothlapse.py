bfc3481b6c1683952e3ef59d968cf8617d4ead08
Andrei Popescu <andy.pope.13@gmail.com>
2019-12-28 15:17:09 -0800

    Non-blocking solving, progress report, memory optimization, configurable threshold, misc fixes...

--- smoothlapse.py
+++ smoothlapse.py
@@ -1,5 +1,6 @@
 import argparse
 from timelapse import TimeLapse
+import time
 
 parser = argparse.ArgumentParser()
 
@@ -7,11 +8,26 @@ parser.add_argument("input", type=str, help="Input video file")
 parser.add_argument("output", type=str, help="Output video file")
 parser.add_argument("groupSize", type=int, help="Frame group size")
 parser.add_argument("lookback", type=int, help="Frame lookback")
+parser.add_argument("-t", "--threshold", type=int, help="Diff threshold",)
 
 args = parser.parse_args()
 
-t = TimeLapse(args.input)
-t.Smooth(args.groupSize, args.lookback)
+print("Loading video...")
+if args.threshold:
+    t = TimeLapse(args.input, threshold=args.threshold)
+else:
+    t = TimeLapse(args.input)
+
+print("Calculating smooth timelapse...")
+t.Smooth(args.groupSize, args.lookback, blocking=False)
+
+while not t.isDone():
+    progress = t.getProgress()
+    print('\r[%s%s] %.2f%%' % ('#' * int(progress / 100 * 20), ' ' * (20 - int(progress / 100 * 20)), progress), end="\r")
+    time.sleep(0.5)
+print("\n", end="")
+
+print("Saving to file...")
 t.SaveResult(args.output)
 
 print("Done.")
--- timelapse.py
+++ timelapse.py
@@ -1,70 +1,146 @@
 import matplotlib.pyplot as plt
 import cv2
+import threading
+from enum import Enum
+
+
+class State(Enum):
+    ready = 1
+    calculating = 2
+    done = 3
 
 
 class TimeLapse:
-    m_colorFrames = []
+    m_path = None
     m_greyFrames = []
     m_resultIndexes = []
     m_frameRate = None
 
-    mk_threshold = 100  # TODO: Adjustable?
+    m_lock = threading.RLock()
+    m_progress = 0  # lock required
+    m_state = None  # lock required
 
-    def __init__(self, path):
-        self.Read_(path)
+    mk_threshold = None
 
-    def Smooth(self, groupSize, lookback):
-        if groupSize < 2:
-            raise Exception("Invalid groupSize")
+    def __init__(self, path, threshold=100):
+        self.m_path = path
+        self.mk_threshold = threshold
 
-        if lookback < 1:
-            raise Exception("Invalid lookback")
+        self.Read_()
 
+        self.m_state = State.ready
+
+    def Smooth(self, groupSize, lookback, blocking=True):
+        with self.m_lock:  # Locking the mutex while calculating would block getProgress
+            if self.m_state is State.calculating:
+                raise Exception("Invalid in this state")
+
+            if groupSize < 2:
+                raise Exception("Invalid groupSize")
+
+            if lookback < 1:
+                raise Exception("Invalid lookback")
+
+            self.m_state = State.calculating
+
+        t = threading.Thread(target=self.Smooth_, args=(groupSize, lookback))
+        t.start()
+        if blocking:
+            t.join()
+        else:
+            pass
+
+    def Smooth_(self, groupSize, lookback):
         best = []
         minDiff = None
         for i in range(groupSize):
-            hist, diff = self.Recurse_(groupSize, lookback, i, [], 0)  # TODO: This can be parallelized.
+            hist, diff = self.Recurse_(groupSize, lookback, i, [],
+                                       0)  # TODO: This can be parallelized. Should use processes, not threads to avoid GIL
             if minDiff is None or diff < minDiff:  # TODO: Handle two minimums?
                 minDiff = diff  # TODO: "diff" is lookback only. At this point, we can lookforward too. Calc lookforward here to make a better decision?
                 best = hist
 
         self.m_resultIndexes = best
 
+        with self.m_lock:
+            self.m_state = State.done
+
+    def getProgress(self):
+        with self.m_lock:
+            if self.m_state is not State.calculating:
+                raise Exception("Invalid in this state")
+
+            return self.m_progress
+
+    def isDone(self):
+        with self.m_lock:
+            return self.m_state is State.done
+
     def SaveResult(self, file):
-        if not self.m_resultIndexes:
-            raise Exception("No result to save")
+        with self.m_lock:
+            if self.m_state is not State.done:
+                raise Exception("Invalid in this state")
 
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        shape = self.m_colorFrames[0].shape
-        out = cv2.VideoWriter(file, fourcc, self.m_frameRate, (shape[1], shape[0]))
-        for i in self.m_resultIndexes:
-            out.write(self.m_colorFrames[i])
+            if not self.m_resultIndexes:
+                raise Exception("No result to save")
 
-        out.release()
+            cap = cv2.VideoCapture(self.m_path)
+            ret, frame = cap.read()
+            colorIdx = 0
+
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            shape = frame.shape
+            out = cv2.VideoWriter(file, fourcc, self.m_frameRate, (shape[1], shape[0]))
+            for i in self.m_resultIndexes:
+                while colorIdx < i:
+                    ret, frame = cap.read()
+                    colorIdx += 1
+
+                out.write(frame)
+
+            out.release()
 
     def PlotResultDiff(self):
-        vals = []
+        with self.m_lock:
+            if self.m_state is not State.done:
+                raise Exception("Invalid in this state")
 
-        for i in self.m_resultIndexes:
-            if i == self.m_resultIndexes[0]:
-                # first frame
-                vals.append(self.Diff_(self.m_resultIndexes[0], self.m_resultIndexes[1]) * 2)
-                continue
+            vals = []
 
-            if i == self.m_resultIndexes[len(self.m_resultIndexes) - 1]:
-                # last frame
-                vals.append(self.Diff_(self.m_resultIndexes[len(self.m_resultIndexes) - 1],
-                                       self.m_resultIndexes[len(self.m_resultIndexes) - 2]) * 2)
-                continue
+            for i in range(len(self.m_resultIndexes)):
+                if i == 0:
+                    # first frame
+                    vals.append(self.Diff_(self.m_resultIndexes[0], self.m_resultIndexes[1]) * 2)
 
-            vals.append(self.Diff_(i, i + 1) + self.Diff_(i, i - 1))
+                elif i == len(self.m_resultIndexes) - 1:
+                    # last frame
+                    vals.append(self.Diff_(self.m_resultIndexes[len(self.m_resultIndexes) - 1],
+                                           self.m_resultIndexes[len(self.m_resultIndexes) - 2]) * 2)
 
-        plt.plot(vals)
-        plt.ylabel('some numbers')
-        plt.show()  # TODO: Plot should not be continuous (dots not lines)
+                else:
+                    vals.append(self.Diff_(self.m_resultIndexes[i], self.m_resultIndexes[i + 1]) +
+                                self.Diff_(self.m_resultIndexes[i], self.m_resultIndexes[i - 1]))  # should we really go both ways?
 
-    def Prune(self, threshold):
-        raise Exception("Not implemented") # TODO
+            plt.plot(vals)
+            plt.ylabel('some numbers')
+            plt.show()  # TODO: Plot should not be continuous (dots not lines)
+
+    def ShowResultDiff(self, threshold):
+        with self.m_lock:
+            if self.m_state is not State.done:
+                raise Exception("Invalid in this state")
+
+            for i in range(len(self.m_resultIndexes)):
+                if i != self.m_resultIndexes[0]:
+                    diff = cv2.absdiff(self.m_greyFrames[self.m_resultIndexes[i]], self.m_greyFrames[self.m_resultIndexes[i - 1]])  # Duplicate from Diff_
+                    ret, diff = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)
+
+                    cv2.imshow('frame', diff)
+
+                    if cv2.waitKey(0) & 0xFF == ord('q'):
+                        break
+
+        cv2.destroyAllWindows()
 
     def Recurse_(self, groupSize, lookback, idx, hist, diff):
         assert idx >= len(hist) * groupSize
@@ -94,21 +170,29 @@ class TimeLapse:
                 minDiff = idiff
 
         diff += minDiff
-        return self.Recurse_(groupSize, lookback, minDiffIdx, hist, diff)  # Only called once, not real recursion. Maybe once we branch or do multiple passes
 
-    def Read_(self, path):
-        cap = cv2.VideoCapture(path)
+        with self.m_lock:
+            self.m_progress += 100 / len(self.m_greyFrames)
+
+        return self.Recurse_(groupSize, lookback, minDiffIdx, hist,
+                             diff)  # TODO: Only called once, not real recursion. Switch to loop
+
+    def Read_(self):
+        cap = cv2.VideoCapture(self.m_path)
 
         self.m_frameRate = cap.get(cv2.CAP_PROP_FPS)
 
-        while (cap.isOpened()):  # TODO: Can this be parallelized? At least the conversion to greyscale
+        while (cap.isOpened()):  # TODO: Can this be parallelized? Pipelined?
             ret, frame = cap.read()
 
             if not ret:
                 break
 
-            self.m_colorFrames.append(frame)
-            self.m_greyFrames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame = cv2.resize(frame, (100, 100))
+            self.m_greyFrames.append(frame)
+
+        cap.release()
 
     def Diff_(self, A, B):
         diff = cv2.absdiff(self.m_greyFrames[A], self.m_greyFrames[B])
